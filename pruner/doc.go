de1627b929ac8ca5ed7efa71f83cd673d6897d2a
Jamie Stephens <js@morphism.com>
2022-05-17 17:57:26 +0000

    Move docs around

--- pruner/doc.go
+++ pruner/doc.go
@@ -5,19 +5,7 @@
 // probably be pretty difficult.  At least one approach is pretty
 // easy: Wrap the current matcher to filter removed patterns from
 // match results and periodically rebuild the matcher from scrach with
-// the live patterns.  More specifically:
-//
-// 1.  Remember patterns that have been added
-// 2. Remember patterns that have been removed (implicitly)
-// 3. Filter MatchedFor...() results to remove any removed patterns
-// 4. Support rebuilding the matcher state periodically with only the
-//    live patterns.
-// 5. Maintain some statistics to help decide when to rebuild
-//
-// The implementation of the set of live patterns is pluggable via a
-// Go interface (State).  The default implementation is a
-// `map[quamina.X]string (MemState)`.  Other implementations can
-// provide persistence.
+// the live patterns.
 //
 // By default, rebuilding is triggered automatically (synchronously
 // currently) during mutations.  The code also supports pluggable
